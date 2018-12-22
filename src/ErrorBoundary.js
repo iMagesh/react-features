dcb666d6f16175535489e5db6f1b54c66f08464e
Magesh <magesh@hash14.com>
2018-12-22 20:02:47 +0530

    Updated files to add Dan's buggyCounter example for error boundaries

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,24 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
 
-export default class ErrorBoundary extends Component {
-  state = {
-    hasError: false
-  };
-
-  // static getDerivedStateFromError(error) {
-  //   // Update state so the next render will show the fallback UI.
-  //   return { hasError: true };
-  // }
-
-  componentDidCatch(error, info) {
-    this.setState({ hasError: true });
-    // log using another service if required
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    });
+    // You can also log error messages to an error reporting service here
   }
 
   render() {
-    if (this.state.hasError) {
-      return <h2>Something went wrong</h2>;
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: "pre-wrap" }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
     }
+    // Normally, just render children
     return this.props.children;
   }
 }
+
+export default ErrorBoundary;
