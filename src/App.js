46b3162b70771716e7d20f8cf4007524b35dd54a
Magesh <magesh@hash14.com>
2018-12-23 01:16:05 +0530

    React portals code added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,83 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import ChildComponent from "./ChildComponent";
+import Modal from "./Modal";
 
-export default function App() {
-  return (
-    <div>
-      <ChildComponent />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    clicked: false
+  };
+
+  handleClick = event => {
+    this.setState(state => ({
+      clicked: !state.clicked
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <p>
+          {this.state.clicked ? "You clicked" : "waiting.."}
+          Open up the browser DevTools to observe that the MODAL is not a child
+          of the root div.
+        </p>
+        <div>
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-toggle="modal"
+            data-target="#exampleModal"
+          >
+            Launch demo modal
+          </button>
+        </div>
+        <Modal>
+          <div
+            className="modal fade"
+            id="exampleModal"
+            tabIndex="-1"
+            role="dialog"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">
+                    Modal title
+                  </h5>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">...</div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button
+                    onClick={this.handleClick}
+                    type="button"
+                    className="btn btn-primary"
+                  >
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(App, document.getElementById("root"));
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ThemeContext from "./theme-context";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-const themes = {
-  light: {
-    foreground: "#000000",
-    background: "#eeeeee"
-  },
-  dark: {
-    foreground: "#ffffff",
-    background: "#222222"
-  },
-  name: "Magesh"
-};
-
-ReactDOM.render(
-  <ThemeContext.Provider value={themes}>
-    <App />
-  </ThemeContext.Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
