7e1e8474a9aaea0af3284c3cd67fcf290dd874e8
Magesh <magesh@sedintechnologies.com>
2019-04-10 17:35:06 +0530

    added render props example from official react doc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class Cat extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      <img
+        src="/cat.png"
+        alt="cat"
+        style={{
+          position: "absolute",
+          left: mouse.x,
+          top: mouse.y,
+          height: "20%"
+        }}
+      />
+    );
+  }
+}
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: "100%" }} onMouseMove={this.handleMouseMove}>
+        {/*
+          Instead of providing a static representation of what <Mouse> renders,
+          use the `render` prop to dynamically determine what to render.
+        */}
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Move the mouse around!</h1>
+        <Mouse render={mouse => <Cat mouse={mouse} />} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MouseTracker />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
