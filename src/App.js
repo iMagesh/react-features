0766fb597451108b3076d7614fcb331075789459
Magesh <magesh@sedintechnologies.com>
2019-04-10 16:18:18 +0530

    using HOC to get mouse coordinates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import withMouse from "./MouseHOC";
 
 class App extends Component {
   render() {
+    // Instead of maintaining our own state,
+    // we get the mouse position as a prop!
+    const { x, y } = this.props.mouse;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ height: "100%" }}>
+        <h1>
+          The mouse position is ({x}, {y})
+        </h1>
       </div>
     );
   }
 }
 
-export default App;
+export default withMouse(App);
