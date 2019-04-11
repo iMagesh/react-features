2eaf7b2ee3c03dc64b83d474ff15e374df5a5518
Magesh <magesh@sedintechnologies.com>
2019-04-11 12:57:29 +0530

    added examples for the update life cycle methods

--- src/index.js
+++ src/index.js
@@ -4,60 +4,35 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { val: 0 };
-    this.update = this.update.bind(this);
+    this.state = { increasing: false };
   }
 
-  componentWillMount() {
-    console.log("Component Will Mount");
-    this.setState({ m: 2 });
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    this.setState({ increasing: nextProps.val > this.props.val });
   }
 
-  componentDidMount() {
-    console.log("Component Did Mount");
-    console.log(ReactDOM.findDOMNode(this));
-
-    // lets try setting something that can be cleaned up in unMount later
-    this.inc = setInterval(this.update, 500);
-    //click on unmount and above will throw error in console if you don't cleaned it during unmount
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 === 0;
   }
 
-  componentWillUnmount() {
-    console.log("Component Will Unmount");
-    clearInterval(this.inc); // without this there will be an error in the console when you try to unmount
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`prevProps: ${prevProps.val}`);
+    console.log(prevState);
+    console.log(this.state);
   }
 
   update() {
-    this.setState({ val: this.state.val + 1 });
-  }
-
-  render() {
-    console.log("render");
-    return (
-      <button onClick={this.update}>{this.state.val * this.state.m}</button>
+    ReactDOM.render(
+      <App val={this.props.val + 1} />,
+      document.getElementById("root")
     );
+  }
+  render() {
+    console.log(this.state.increasing);
+    return <button onClick={this.update.bind(this)}>{this.props.val}</button>;
   }
 }
 
-class Wrapper extends React.Component {
-  mount() {
-    ReactDOM.render(<App />, document.getElementById("a"));
-  }
+App.defaultProps = { val: 0 };
 
-  unmount() {
-    ReactDOM.unmountComponentAtNode(document.getElementById("a"));
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.mount.bind(this)}>Mount</button>
-        <button onClick={this.unmount.bind(this)}>UnMount</button>
-      </div>
-    );
-  }
-}
-
-export default Wrapper;
-
-ReactDOM.render(<Wrapper />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
